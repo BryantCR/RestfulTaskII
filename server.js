4f4293122f52d25c801fc6c7125caee89cbd5d00
BryantCR <bryancvargas20@gmail.com>
2021-11-26 14:27:11 -0600

    Ok

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const {ApiRouter} = require( './server/routes/ApiRouter' );
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('./server/config/database');
+app.use( '', ApiRouter );
+
+app.listen( 8080, function(){
+    console.log( "The users server is running in port 8080." );
+});
